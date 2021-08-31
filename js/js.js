791d07dd8ef0437050299051e04cdf2d02a4c40a
Eric-000 <ericxu000@gmail.com>
2021-08-31 12:17:27 +1000

    update commit

--- js/js.js
+++ js/js.js
@@ -1,4 +1,28 @@
-function myFunction() {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
- }
+const toggleButton = document.querySelector('#toggle-button')
+  
+toggleButton.addEventListener('click', (e) => {
+  darkMode = localStorage.getItem('theme');
+  if (darkMode === 'dark') {
+    disableDarkMode();
+  } else {
+    enableDarkMode();
+  }
+});
+
+function enableDarkMode() {
+  document.body.classList.add("dark-mode")
+  bform.classList.add("dark-mode")
+  bookingform.classList.add("dark-mode")
+  pform.classList.add("dark-mode")
+  uform.classList.add("dark-mode")
+  localStorage.setItem('theme', 'dark');
+}
+
+function disableDarkMode() {
+  document.body.classList.remove("dark-mode")
+  localStorage.setItem('theme', 'light');
+}
+
+let darkMode = localStorage.getItem("theme")
+
+if (darkMode === "dark") enableDarkMode()
